4c7c2e5c4a081353390b3381d5d1ce391b0e3049
Jouni Kiviperä <jounikivipera@gmail.com>
2022-04-02 07:41:01 +0300

    aloitus

---
+++ go-server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func  formHandler
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+}
+if r.Method != "GET" {
+	http.Error(w, "Method not allowed", http.StatusNotFound)
+	return
+}
+fmt.Fprintf(w, "Hello!")
+}
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
