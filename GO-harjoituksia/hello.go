c5c7f23179284b223588d7a50207ad5f74b755b7
Jouni Kiviperä <jounikivipera@gmail.com>
2022-06-03 09:10:03 +0300

    Update hello.go

--- GO-harjoituksia/hello.go
+++ GO-harjoituksia/hello.go
@@ -10,8 +10,20 @@ func main() {
 	// variables and Constants
 	// strings, int, float32, bool
 
-	var firstName string = "Jouni"
+	//var firstName string = "Jouni"
 
-	fmt.Println(firstName)
+	// := shorthand variables
+
+	//lastName := "Kiviperä"
+	//age := 41
+
+	// Create but dont's assign
+
+	var fullName string
+	var age int
+	var price float32
+	var tf bool
+
+	fmt.Println(fullName, age, price, tf)
 
 }
