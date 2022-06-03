30542b3d46fc6e31fc560444b479ae94b503f951
Jouni Kiviperä <jounikivipera@gmail.com>
2022-06-03 09:02:58 +0300

    muutoksia

---
+++ GO-harjoituksia/hello.go
@@ -0,0 +1,17 @@
+package main
+
+// import variables
+import (
+	"fmt"
+)
+
+func main() {
+
+	// variables and Constants
+	// strings, int, float32, bool
+
+	var firstName string = "Jouni"
+
+	fmt.Println(firstName)
+
+}
